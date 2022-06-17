2687c07d1ba5a0a4b95c4df5d0cb40ba1c8307f0
Harry Anderson <harryanderson9@gmail.com>
2022-06-17 11:40:27 +0000

    websocket connecting

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,4 @@
+use ewebsock::{WsEvent, WsMessage, WsReceiver, WsSender};
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
@@ -8,6 +9,10 @@ pub struct TemplateApp {
     // this how you opt-out of serialization of a member
     #[serde(skip)]
     value: f32,
+    url: String,
+    error: String,
+    #[serde(skip)]
+    frontend: Option<FrontEnd>,
 }
 
 impl Default for TemplateApp {
@@ -16,6 +21,9 @@ impl Default for TemplateApp {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            error: "".to_string(),
+            url: "".to_string(),
+            frontend: None,
         }
     }
 }
@@ -25,6 +33,7 @@ impl TemplateApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customized the look at feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+        //
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
@@ -33,6 +42,19 @@ impl TemplateApp {
         }
 
         Default::default()
+    }
+    fn connect(&mut self, ctx: egui::Context) {
+        let wakeup = move || ctx.request_repaint(); // wake up UI thread on new message
+        match ewebsock::connect_with_wakeup(&self.url, wakeup) {
+            Ok((ws_sender, ws_receiver)) => {
+                self.frontend = Some(FrontEnd::new(ws_sender, ws_receiver));
+                self.error.clear();
+            }
+            Err(error) => {
+                println!("Failed to connect to {:?}: {}", &self.url, error);
+                self.error = error;
+            }
+        }
     }
 }
 
@@ -45,67 +67,86 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
-
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
         // Tip: a good default choice is to just keep the `CentralPanel`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
-
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        frame.quit();
-                    }
+        if !frame.is_web() {
+            egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+                egui::menu::bar(ui, |ui| {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Quit").clicked() {
+                            frame.quit();
+                        }
+                    });
                 });
-            });
-        });
-
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
-            });
-        });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
-        });
-
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
             });
         }
+
+        egui::TopBottomPanel::top("server").show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.label("URL:");
+                if ui.text_edit_singleline(&mut self.url).lost_focus()
+                    && ui.input().key_pressed(egui::Key::Enter)
+                {
+                    self.connect(ctx.clone());
+                }
+            });
+        });
+
+        if !self.error.is_empty() {
+            egui::TopBottomPanel::top("error").show(ctx, |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("Error:");
+                    ui.colored_label(egui::Color32::RED, &self.error);
+                });
+            });
+        }
+
+        if let Some(frontend) = &mut self.frontend {
+            frontend.ui(ctx);
+        }
+    }
+}
+
+struct FrontEnd {
+    ws_sender: WsSender,
+    ws_receiver: WsReceiver,
+    events: Vec<WsEvent>,
+    text_to_send: String,
+}
+
+impl FrontEnd {
+    fn new(ws_sender: WsSender, ws_receiver: WsReceiver) -> Self {
+        Self {
+            ws_sender,
+            ws_receiver,
+            events: Default::default(),
+            text_to_send: Default::default(),
+        }
+    }
+
+    fn ui(&mut self, ctx: &egui::Context) {
+        while let Some(event) = self.ws_receiver.try_recv() {
+            self.events.push(event);
+        }
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.label("Message to send:");
+                if ui.text_edit_singleline(&mut self.text_to_send).lost_focus()
+                    && ui.input().key_pressed(egui::Key::Enter)
+                {
+                    self.ws_sender
+                        .send(WsMessage::Text(std::mem::take(&mut self.text_to_send)));
+                }
+            });
+
+            ui.separator();
+            ui.heading("Received events:");
+            for event in &self.events {
+                ui.label(format!("{:?}", event));
+            }
+        });
     }
 }
